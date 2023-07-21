d4f4385643e259e4d61618dcb96aafac7bcfc71b
jihedmrd <jihed.mrad.96@gmail.com>
2023-07-21 19:25:22 +0100

    Front-End

--- src/main/java/com/confledis/miniProject/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.confledis.miniProject;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
